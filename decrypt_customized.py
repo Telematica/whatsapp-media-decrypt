9aef7b4aeff3beaf2193a8f560776b03d2c0885d
Telematica <linuxeron@gmail.com>
2021-09-17 13:57:40 -0500

    WIP

--- decrypt_customized.py
+++ decrypt_customized.py
@@ -7,36 +7,54 @@ import sys
 
 fileName = sys.argv[1]
 mediaKey = sys.argv[2]
+type = sys.argv[3]
+
+if type == "video":
+    x = b"WhatsApp Video Keys"
+elif type == "image":
+    x = b"WhatsApp Image Keys"
+elif type == "audio":
+    x = b"WhatsApp Audio Keys"
+
+print("x")
+print(x)
 
 def HKDF(key, length, appInfo=b""):
-    key = hmac.new(b"\0"*32, key, hashlib.sha256).digest()
+    key = hmac.new(b"\0" * 32, key, hashlib.sha256).digest()
     keyStream = b""
     keyBlock = b""
     blockIndex = 1
     while len(keyStream) < length:
-        keyBlock = hmac.new(key, msg=keyBlock+appInfo+(chr(blockIndex).encode("utf-8")), digestmod=hashlib.sha256).digest()
+        keyBlock = hmac.new(
+            key,
+            msg=keyBlock + appInfo + (chr(blockIndex).encode("utf-8")),
+            digestmod=hashlib.sha256,
+        ).digest()
         blockIndex += 1
         keyStream += keyBlock
     return keyStream[:length]
 
+
 def AESUnpad(s):
-    return s[:-ord(s[len(s)-1:])];
+    return s[: -ord(s[len(s) - 1 :])]
+
 
 def AESDecrypt(key, ciphertext, iv):
-    cipher = AES.new(key, AES.MODE_CBC, iv);
-    plaintext = cipher.decrypt(ciphertext);
-    return AESUnpad(plaintext);
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = cipher.decrypt(ciphertext)
+    return AESUnpad(plaintext)
 
-mediaKeyExpanded=HKDF(base64.b64decode(mediaKey),112,b"WhatsApp Image Keys")
-macKey=mediaKeyExpanded[48:80]
-mediaData=open(fileName,"rb").read()
 
-file= mediaData[:-10]
-mac= mediaData[-10:]
+mediaKeyExpanded = HKDF(base64.b64decode(mediaKey), 112, x)
+macKey = mediaKeyExpanded[48:80]
+mediaData = open(fileName, "rb").read()
 
-imgdata=AESDecrypt(mediaKeyExpanded[16:48],file, mediaKeyExpanded[:16])
+file = mediaData[:-10]
+mac = mediaData[-10:]
 
-with open(fileName.replace('.enc', '.bin'), 'wb') as f:
+imgdata = AESDecrypt(mediaKeyExpanded[16:48], file, mediaKeyExpanded[:16])
+
+with open(fileName.replace(".enc", ".bin"), "wb") as f:
     f.write(imgdata)
 
-print("Decrypted image (hopefully)")
+print(f"Decrypted {type} (hopefully)")
--- script.js
+++ script.js
@@ -1,45 +1,110 @@
-const Doc = require("./2021-08-27.json");
-const http = require('https'); // or 'https' for https:// URLs
-const fs = require('fs');
-const { exec } = require('child_process');
+const http = require("https"); // or 'https' for https:// URLs
+const fs = require("fs");
+const { exec } = require("child_process");
+const { url } = require("inspector");
+const { argv } = require("process");
 
+if (argv[0]) {
+  console.log("No file!");
+  return;
+}
+
+const Doc = require(`./${argv[0]}`);
+
+
+// https://ali-dev.medium.com/how-to-use-promise-with-exec-in-node-js-a39c4d7bbf77
 
 (async () => {
-    await (async () => { Doc.forEach(async (item) => {
-        const type = item.type;
-        if (type === "chat") {
-            console.log(item.body);
-            return;
-        }
-        const encFile = item.deprecatedMms3Url.match(/\b\/\w.*\.enc\b/)[0].replace('/d/f/', '');
-        console.log(encFile, item.mediaKey, `python3 decrypt.py ${encFile} "${item.mediaKey}"`);
-        
-        exec(`curl -O ${item.deprecatedMms3Url}`,
-        (error, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
+  const files = Doc.map((file) => {
+    return {
+      body: file.body,
+      deprecatedMms3Url: file.deprecatedMms3Url,
+      mediaKey: file.mediaKey,
+      type: file.type,
+      url: file.deprecatedMms3Url
+        .match(/\b\/\w.*\.enc\b/)[0]
+        .replace("/d/f/", ""),
+    };
+  });
 
-        exec(`python ./decrypt.py ${encFile} "${item.mediaKey}"`,
-            (error, stdout, stderr) => {
-                console.log(stdout);
-                console.log(stderr);
-                if (error !== null) {
-                    console.log(`exec error: ${error}`);
-                }
-        });
-        });
-    })();
+  const downloadFile = (url) => new Promise((resolve, reject) => {
+    console.log(`Downloading... curl -O ${url}`);
+    exec(`curl -O ${url}`, (error, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      if (error !== null) {
+        console.log(`exec error: ${error}`);
+      }
+      resolve(stdout ? stdout : stderr);
+    });
+  });
 
-    exec(`for i in ./*.bin ; do mv "\${i}" "\${i}.jpeg" ; done`,
-    (error, stdout, stderr) => {
+  const decryptFile = (file, mediaKey, type) => new Promise((resolve, reject) => {
+    console.log(`Decrypting... python3 decrypt_customized.py ${file} "${mediaKey}" ${type}`);
+    exec(
+      `python3 ./decrypt_customized.py ${file} "${mediaKey}" ${type}`,
+      (error, stdout, stderr) => {
         console.log(stdout);
         console.log(stderr);
         if (error !== null) {
-            console.log(`exec error: ${error}`);
+          console.log(`exec error: ${error}`);
         }
-    });
+        resolve(stdout ? stdout : stderr);
+      }
+    );
+  });
+  
+  const renameFile = (file, extension) => {
+    exec(
+      `mv ./${file}.bin "${file}.${extension}"`,
+      (error, stdout, stderr) => {
+        console.log(stdout);
+        console.log(stderr);
+        if (error !== null) {
+          console.log(`exec error: ${error}`);
+        }
+      }
+    );
+  };
+
+  const renameFileBulk = (extension) => {
+    exec(
+      `for i in ./*.bin ; do mv "\${i}" "\${i}.${extension}" ; done`,
+      (error, stdout, stderr) => {
+        console.log(stdout);
+        console.log(stderr);
+        if (error !== null) {
+          console.log(`exec error: ${error}`);
+        }
+      }
+    );
+  };
+
+  const extension = (type) => {
+    switch (type) {
+      case "audio":
+        return "ogg";
+      case "image": 
+        return "jpeg";
+      case "video":
+        return "mp4";
+      default:
+        return "jpeg";
+    }
+  };
+
+  const downloadDecryptAndRenameFile = async (file) => {
+    const { type } = file;
+    if (type === "chat") {
+      console.log(file.body);
+      return;
+    }
+    await downloadFile(file.deprecatedMms3Url);
+    await decryptFile(file.url, file.mediaKey, type);
+  };
+
+  await Promise.all(
+    files.map((file) => downloadDecryptAndRenameFile(file))
+  );
+  files.map((file) => renameFile(file.url.replace(".enc", ""), extension(file.type)));
 })();
